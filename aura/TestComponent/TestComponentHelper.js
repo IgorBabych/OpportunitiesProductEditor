f8ab3c7e849c93b1bbeddd391fd3393e013eda8e
igorbabych4 <igorbabych4@gmail.com>
2019-06-21 13:37:12 +0300

    refactoring

--- aura/TestComponent/TestComponentHelper.js
+++
@@ -1,9 +0,0 @@
-({
-    removeLead : function(component, index) {
-        var leads = component.get("v.expenses");
-
-        leads.splice(index, 1);
-        component.set("v.expenses", leads);
-
-    }
-})
